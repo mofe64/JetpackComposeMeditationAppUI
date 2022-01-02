13b758b7e6bded59402153120099c7ef193478d1
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2022-01-02 08:05:04 +0100

    greeting section and chip section

---
+++ app/src/main/java/com/nubari/meditationui/HomeScreen.kt
@@ -0,0 +1,101 @@
+package com.nubari.meditationui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.nubari.meditationui.ui.theme.ButtonBlue
+import com.nubari.meditationui.ui.theme.DarkerButtonBlue
+import com.nubari.meditationui.ui.theme.DeepBlue
+import com.nubari.meditationui.ui.theme.TextWhite
+
+@Composable
+fun HomeScreen() {
+    Box(
+        modifier = Modifier
+            .background(DeepBlue)
+            .fillMaxSize()
+    ) {
+        Column {
+            GreetingSection()
+        }
+    }
+}
+
+@Composable
+fun GreetingSection(
+    name: String = "Mofe"
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp)
+    ) {
+        Column(verticalArrangement = Arrangement.Center) {
+            Text(
+                text = "Good morning $name",
+                style = MaterialTheme.typography.h2
+            )
+            Text(
+                text = "We wish you have a good day",
+                style = MaterialTheme.typography.body1
+            )
+        }
+        Icon(
+            painter = painterResource(id = R.drawable.ic_search),
+            contentDescription = "Search",
+            tint = Color.White,
+            modifier = Modifier.size(24.dp)
+        )
+    }
+}
+
+@Composable
+fun ChipSection(
+    chips: List<String>
+) {
+    var selectedChipIndex by remember {
+        mutableStateOf(0)
+    }
+
+    LazyRow {
+        items(chips.size) {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier =
+                Modifier
+                    .padding(
+                        start = 15.dp,
+                        top = 15.dp,
+                        bottom = 15.dp
+                    )
+                    .clickable {
+                        //it comes from the item scope and refers to current Index
+                        selectedChipIndex = it
+                    }
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(
+                        if (selectedChipIndex == it) ButtonBlue
+                        else DarkerButtonBlue
+                    )
+                    .padding(15.dp)
+            ) {
+                Text(text = chips[it], color = TextWhite)
+            }
+
+        }
+    }
+}
