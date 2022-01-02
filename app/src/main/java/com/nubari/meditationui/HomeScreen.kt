824dcdc795ec06991219a5d413e2721c3aa94c6f
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2022-01-02 11:37:32 +0100

    grid and grid components

--- app/src/main/java/com/nubari/meditationui/HomeScreen.kt
+++ app/src/main/java/com/nubari/meditationui/HomeScreen.kt
@@ -3,10 +3,14 @@ package com.nubari.meditationui
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
@@ -16,11 +20,17 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.nubari.meditationui.ui.theme.*
+import kotlin.math.abs
 
+@ExperimentalFoundationApi
 @Composable
 fun HomeScreen() {
     Box(
@@ -32,6 +42,52 @@ fun HomeScreen() {
             GreetingSection()
             ChipSection(chips = listOf("Lagos", "Android", "London", "Detty December"))
             CurrentMeditation()
+            FeatureSection(
+                features = listOf(
+                    Feature(
+                        "Money meditation",
+                        R.drawable.ic_headphone,
+                        BlueViolet1,
+                        BlueViolet2,
+                        BlueViolet3
+                    ),
+                    Feature(
+                        "Sleep meditation 2",
+                        R.drawable.ic_headphone,
+                        BlueViolet1,
+                        BlueViolet2,
+                        BlueViolet3
+                    ),
+                    Feature(
+                        "Tips for securing the bag",
+                        R.drawable.ic_headphone,
+                        LightGreen1,
+                        LightGreen2,
+                        LightGreen3
+                    ),
+                    Feature(
+                        "Calming Sounds",
+                        R.drawable.ic_headphone,
+                        OrangeYellow1,
+                        OrangeYellow2,
+                        OrangeYellow3
+                    ),
+                    Feature(
+                        "Calm sounds 2",
+                        R.drawable.ic_headphone,
+                        Beige1,
+                        Beige2,
+                        Beige3
+                    ),
+                    Feature(
+                        "Tips for securing the bag",
+                        R.drawable.ic_headphone,
+                        BlueViolet1,
+                        BlueViolet2,
+                        BlueViolet3
+                    ),
+                )
+            )
         }
     }
 }
@@ -151,4 +207,174 @@ fun CurrentMeditation(
             )
         }
     }
+}
+
+@ExperimentalFoundationApi
+@Composable
+fun FeatureSection(
+    features: List<Feature>
+) {
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Text(
+            text = "Features",
+            style = MaterialTheme.typography.h1,
+            modifier = Modifier.padding(15.dp)
+        )
+        LazyVerticalGrid(
+            cells = GridCells.Fixed(2),
+            contentPadding = PaddingValues(start = 7.5.dp, end = 7.5.dp, bottom = 100.dp),
+            modifier = Modifier.fillMaxHeight()
+
+        ) {
+            items(features.size) {
+                FeatureItem(feature = features[it])
+            }
+        }
+    }
+}
+
+@Composable
+fun FeatureItem(
+    feature: Feature
+) {
+    BoxWithConstraints(
+        modifier = Modifier
+            .padding(7.5.dp)
+            .aspectRatio(1f) //ensures that whatever the width is, the same value is assigned to the height, so we essentially have a square
+            .clip(RoundedCornerShape(10.dp))
+            .background(feature.darkColor)
+    ) {
+        // because we are using a box with constraints, we can get the constraints of our composable
+        val width = constraints.maxWidth
+        val height = constraints.maxHeight
+
+        /*medium colored path
+        * we use our offsets to set coordinates on the composable that we want
+        * to map out
+        * for point 1 our x value is 1 that means top left of the composable
+        * the y value for point 1 is height * 0.3f this means we want our
+        * y value to be start at 30% of the height of the composable which is
+        * height * (30/100) this gives us height * 0.3f
+        * We use the same logic to capture different points on the composable
+        */
+        val mediumColoredPoint1 = Offset(0f, height * 0.3f)
+        val mediumColoredPoint2 = Offset(width * 0.1f, height * 0.35f)
+        val mediumColoredPoint3 = Offset(width * 0.4f, height * 0.5f)
+        val mediumColoredPoint4 = Offset(width * 0.75f, height * 0.7f)
+        val mediumColoredPoint5 = Offset(width * 1.4f, -height.toFloat())
+        /*
+        * Now we want to join all our points together to create a path along
+        * the composable
+        * we start our path at our point1 so we move to point1,
+        * then we use a quadratic bezier which is a mathematical way
+        * of drawing curves
+        * in the method the first two args represent the point we want to draw the
+        * path to, while the next two values describe how the curve will look like
+        * A safe way to make the curve look good, is to take the two points this
+        * section of the path is connecting, that is our start point and end point
+        * for the curve, add them together and divide by 2
+        * Example
+        * quadraticBezierTo(
+                mediumColoredPoint2.x,
+                mediumColoredPoint2.y,
+                abs(mediumColoredPoint1.x + mediumColoredPoint2.x) / 2f,
+                abs(mediumColoredPoint1.y + mediumColoredPoint2.y) / 2f
+            )
+        * We abstracted above functionality into a helper method
+        * called standardQuadFromTo() which was
+        * added as an extension of the Path object so we have access to it in
+        * the path scope
+        * */
+        val mediumColoredPath = Path().apply {
+            moveTo(mediumColoredPoint1.x, mediumColoredPoint1.y)
+            standardQuadFromTo(mediumColoredPoint1, mediumColoredPoint2)
+            standardQuadFromTo(mediumColoredPoint2, mediumColoredPoint3)
+            standardQuadFromTo(mediumColoredPoint3, mediumColoredPoint4)
+            standardQuadFromTo(mediumColoredPoint4, mediumColoredPoint5)
+            /*
+            * Next we want to convert our path into an area that we can fill
+            * with color, we essentially draw lines from the last point on our curve
+            * to the values specified below
+            * so if the last point on our curve is 16, we move along the x axis
+            * by width.toFloat() + 100f then along the y axis by height.toFloat() + 100f
+            * then we move along the x axis again by -100f then along the y axis
+            * by height.toFloat() + 100f)
+            * the 100f values we are adding are just arbitrary values we are adding to ensure
+            * that the points we are moving to are definitely outside of our composable.
+            * then we call join to close the oath and turn into an area.
+            * so we can fill the area with color,
+            * since the composable is clipped, the areas outside the composable will
+            * not be visible. its basically like overflow hidden on css
+            * */
+            lineTo(width.toFloat() + 100f, height.toFloat() + 100f)
+            lineTo(-100f, height.toFloat() + 100f)
+            close()
+        }
+
+        //light colored path
+        val lightPoint1 = Offset(0f, height * 0.35f)
+        val lightPoint2 = Offset(width * 0.1f, height * 0.4f)
+        val lightPoint3 = Offset(width * 0.3f, height * 0.35f)
+        val lightPoint4 = Offset(width * 0.65f, height.toFloat())
+        val lightPoint5 = Offset(width * 1.4f, -height / 3f)
+
+        val lightColoredPath = Path().apply {
+            moveTo(lightPoint1.x, lightPoint1.y)
+            standardQuadFromTo(lightPoint1, lightPoint2)
+            standardQuadFromTo(lightPoint2, lightPoint3)
+            standardQuadFromTo(lightPoint3, lightPoint4)
+            standardQuadFromTo(lightPoint4, lightPoint5)
+            lineTo(width.toFloat() + 100f, height.toFloat() + 100f)
+            lineTo(-100f, height.toFloat() + 100f)
+            close()
+        }
+        //We use our canvas to draw out the path and fill it with color
+        Canvas(modifier = Modifier.fillMaxSize()) {
+            drawPath(
+                path = mediumColoredPath,
+                color = feature.mediumColor
+            )
+            drawPath(
+                path = lightColoredPath,
+                color = feature.lightColor
+            )
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(15.dp)
+        ) {
+            Text(
+                text = feature.title,
+                style = MaterialTheme.typography.h1,
+                lineHeight = 26.sp,
+                modifier = Modifier.align(Alignment.TopStart)
+            )
+            Icon(
+                painter = painterResource(
+                    id = feature.iconId
+                ),
+                contentDescription = feature.title,
+                tint = Color.White,
+                modifier = Modifier.align(Alignment.BottomStart)
+            )
+            Text(
+                text = "Start",
+                color = TextWhite,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .clickable { }
+                    .align(Alignment.BottomEnd)
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(ButtonBlue)
+                    .padding(vertical = 6.dp, horizontal = 15.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun BottomMenu() {
+
 }
